6158b1b7e263cb453864750d04376180e47fc739
TesteAc <113122270+TesteAc@users.noreply.github.com>
2022-10-06 15:03:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GA3!
+          GA4!
         </p>
         <a
           className="App-link"
