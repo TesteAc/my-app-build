f273746830d64f4729a83b2edecea0fe22d41ba7
TesteAc <113122270+TesteAc@users.noreply.github.com>
2022-10-07 15:25:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GAlbuquerque!
+          GAlbuquerque teste!
         </p>
         <a
           className="App-link"
